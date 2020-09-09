6136a122ac39d157c415b43ec66f1837c30b42d5
alhoff <andrew.hoff14@yahoo.com>
2020-09-09 00:11:43 -0700

    added timer and prompt for player's initials

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ let answerBtn1 = $(".answer1");
 let answerBtn2 = $(".answer2");
 let answerBtn3 = $(".answer3");
 let answerBtn4 = $(".answer4");
-let timeLeft = 60;
+let timeLeft = 5;
 
-setInterval(function() {
+let timer = setInterval(function() {
     timeLeft--
     timeLeftEl.text("Time left: " + timeLeft);
+    if (timeLeft === 0) {
+        clearInterval(timer);
+        endQuiz();
+    }
 },1000)
+
+function endQuiz() {
+    let initials = prompt("You earned a score of " + timeLeft + "!\nPlease enter your initials for the leaderboards");
+}
